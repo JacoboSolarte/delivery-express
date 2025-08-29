c864da21edc4e069bea0d813d26deb46366f8710
JacoboSolarte <jacobo.solarte@campusucc.edu.co>
2025-08-29 11:29:52 -0500

    Logica en pedido.java

---
+++ demo/src/main/java/com/example/coleciones/Ejercicios.java
@@ -0,0 +1,5 @@
+package com.example.coleciones;
+
+public class Ejercicios {
+    
+}
--- demo/src/main/java/com/example/modelo/Pedido.java
+++ demo/src/main/java/com/example/modelo/Pedido.java
@@ -1,5 +1,34 @@
 package com.example.modelo;
 
 public class Pedido {
-    
+    private String producto;
+    private double total;
+    private String cliente;
+
+    public Pedido(String producto, double total, String cliente) {
+        this.producto = producto;
+        this.total = total;
+        this.cliente = cliente;
+    }
+
+    public String getProducto() {
+        return producto;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public String getCliente() {
+        return cliente;
+    }
+
+    @Override
+    public String toString() {
+        return "Pedido{" +
+                "producto='" + producto + '\'' +
+                ", total=" + total +
+                ", cliente='" + cliente + '\'' +
+                '}';
+    }
 }
